f512509ddea83cd4dda1a51907d79b585e917939
Ashu-10-tech <ashunegi1425@gmail.com>
2025-11-13 23:22:45 +0530

    .

--- contract-farming-backend/server.js
+++ contract-farming-backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors =  require('cors'); // Use const cors = require('cors')
+const cors = require('cors'); // Correct way to import cors
 
 dotenv.config();
 const app = express();
